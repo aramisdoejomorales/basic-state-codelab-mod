69eb622f8a8ac15b1264594fb4608add4aa97132
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-18 23:49:16 -0400

    Add .

--- app/src/main/java/com/adoejosld/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/adoejosld/basicstatecodelab/WaterCounter.kt
@@ -1,32 +1,52 @@
 package com.adoejosld.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        val counter: MutableState<Int> = remember { mutableIntStateOf(0) }
-        Text(text = "You've had ${counter.value} glasses.")
-        Button(onClick = { counter.value++ }, modifier = modifier.padding(top = 8.dp)) {
-            Text(text = "Add One")
+    Column(modifier = Modifier.padding(10.dp)) {
+        var counter by remember { mutableIntStateOf(0) }
+        if (counter > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false })
+            }
+            Text(text = "You've had $counter glasses.")
         }
-        Button(onClick = { counter.value = 0 }, modifier = modifier.padding(top = 8.dp)) {
-            Text(text = "Clear")
+        Row(modifier = modifier.padding(top = 10.dp), verticalAlignment = Alignment.CenterVertically) {
+            Button(
+                onClick = { counter++ },
+                enabled = counter < 10
+            ) {
+                Text(text = "Add One")
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(onClick = { counter = 0 }) {
+                Text(text = "Clear Water Counter")
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun WaterCounterPreview() {
     WaterCounter()
