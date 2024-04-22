92d7f99ad4ef3bece96c3fac351110f31d9369ef
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-22 00:04:32 -0400

    Add .

--- app/src/main/java/com/adoejosld/basicstatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/adoejosld/basicstatecodelab/WellnessTasksList.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
-fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+fun getWellnessTasks() = List(40) { i -> WellnessTask(i, "Task # $i") }
 
 @Composable
 fun WellnessTaskList(
