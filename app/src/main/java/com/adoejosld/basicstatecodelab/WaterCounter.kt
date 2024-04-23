3c1419a245b33db1114ebf2fccdc84e10cbd19f5
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-22 22:37:26 -0400

    Add .

--- app/src/main/java/com/adoejosld/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/adoejosld/basicstatecodelab/WaterCounter.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
