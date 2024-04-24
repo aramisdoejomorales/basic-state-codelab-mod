9c55eb9de5e456c7bc73bf57f6a108b0a7696ab8
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-23 23:02:52 -0400

    Add .

--- app/src/main/java/com/adoejosld/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/adoejosld/basicstatecodelab/WellnessScreen.kt
@@ -4,19 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter()
         Spacer(modifier = Modifier.height(12.dp))
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTaskList(list = list, onCloseTask = { task -> list.remove(task) }, modifier)
+        WellnessTaskList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task) },
+            modifier
+        )
     }
 }
 
-private fun getWellnessTasks() = List(40) { i -> WellnessTask(i, "Task # $i") }
